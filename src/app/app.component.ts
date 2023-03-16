318f50993049a6945de1a70c69404d9a39740f40
mahfuzswe <mahfuzur.web@daffodilvarsity.edu.bd>
2023-03-16 15:51:18 +0600

    personal details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,37 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   title = 'angular-form';
+  defaultCountry = 'bangladesh';
+  firstname: string | undefined;
+  lastname: string | undefined;
+  defaultGender = 'Male';
+
+  gender:
+    | [
+        { id: 1; value: 'Male' },
+        { id: 2; value: 'Female' },
+        { id: 3; value: 'Other' }
+      ]
+    | undefined;
   @ViewChild('myForm') form: NgForm | undefined;
 
   onSubmit() {
     console.log(this.form);
+  }
+  setDefaultValues() {
+    //this.form.value.perspnalDetails.firstname = 'JOhn';
+    // this.form?.setValue({
+    //   country: '',
+    //   gender: '',
+    //   hibbies: '',
+    //   personalDetails: {
+    //     firstname: 'vc',
+    //     lastname: 'c',
+    //     email: '',
+    //   },
+    // });
+    this.form?.form.patchValue({
+      personalDetails: {},
+    });
   }
 }
